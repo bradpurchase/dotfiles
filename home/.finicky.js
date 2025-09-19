68796c5efc793ba757a6b1d78a1f6a2600af1740
Brad Purchase <bradpurchase@gmail.com>
2025-09-19 07:28:02 -0400

    Update .finicky.js

--- home/.finicky.js
+++ home/.finicky.js
@@ -4,27 +4,5 @@ export default {
     // Hide the Finicky icon from the menu bar
     hideIcon: false,
   },
-  handlers: [
-    {
-      match: "github.com/GetJobber/*",
-      browser: {
-        name: "Google Chrome",
-        profile: "Work",
-      },
-    },
-    {
-      match: "split.io/*",
-      browser: {
-        name: "Google Chrome",
-        profile: "Work",
-      },
-    },
-    {
-      match: "jobber.atlassian.net/*",
-      browser: {
-        name: "Google Chrome",
-        profile: "Work",
-      },
-    },
-  ],
+  handlers: [],
 };
